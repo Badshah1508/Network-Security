f719274e33f62740f2db81b244d4293d58333f87
Badshah1508 <badshahkumar1508@gmail.com>
2026-01-14 22:06:07 +0530

    all the dependencies are install inside the virtual environment

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import find_packages, setup
 from typing import List
 
-def get_requirements()->List[str]:
+def get_requirements() -> List[str]:
     """
     This function will return list of requirements
     """
-    requirement_lst : List[str]
+    requirement_lst: List[str] = [] 
+
     try:
-        with open('requirements.txt','r')as file:
-            # Read lines from the file
+        with open('requirements.txt', 'r') as file:
             lines = file.readlines()
-            # Process each line
             for line in lines:
-                requirement= line.strip()
+                requirement = line.strip()
                 # Ignore empty lines and -e .
-                if requirement and requirement!=  '-e .':
+                if requirement and requirement != '-e .':
                     requirement_lst.append(requirement)
     except FileNotFoundError:
         print("requirements.txt file is not found")
-        
+
+    return requirement_lst   
+
 setup(
     name="NetworkSecurity",
     version="0.0.1",
